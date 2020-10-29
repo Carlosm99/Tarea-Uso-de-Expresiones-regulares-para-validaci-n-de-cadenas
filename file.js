bd28addf6f65b25b82d4c52fe15151c3d263c76b
carlos monterrosa <carlosmontess07@gmail.com>
2020-10-29 16:37:07 -0500

    alert for checking data

--- file.js
+++ file.js
@@ -2,17 +2,21 @@
 function validarelformulario(){
     valorpass = document.getElementById("password").value;
     valorcolor = document.getElementById("color").value;
+    
     var regexpass = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[$@$!%*?&])([A-Za-z\d$@$!%*?&]|[^ ]){12,100}$/;
     var regexpcolor = /hsl\(\s*(\d+)\s*,\s*(\d+(?:\.\d+)?%)\s*,\s*(\d+(?:\.\d+)?%)\)/g;
     
-    var reusltadopass = regexpass.test(valorpass);       
+    var reusltadopass = regexpass.exec(valorpass);       
 
     var resltadocolor = regexpcolor.exec(valorcolor).slice(1);
      
-    alert("Color:"+reusltadopass+ "\nHue: " + resltadocolor[0] + "\nSaturation: " + resltadocolor[1] + "\nValue: " + resltadocolor[2]);      
+    alert("Contraseña :"+reusltadopass+ "\nMatiz: " + resltadocolor[0] + "\nSaturación: " + resltadocolor[1] + "\nValor: " + resltadocolor[2]);      
     
     return false;
+
+    
 }
+
 
 
 
