086b8562b2b2a419267d16bf5c1ddaf4ced6c24c
carlos monterrosa <carlosmontess07@gmail.com>
2020-10-29 18:03:45 -0500

    convert object to string from exec()

--- file.js
+++ file.js
@@ -6,12 +6,16 @@ function validarelformulario(){
     var regexpass = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[$@$!%*?&])([A-Za-z\d$@$!%*?&]|[^ ]){12,100}$/;
     var regexpcolor = /hsl\(\s*(\d+)\s*,\s*(\d+(?:\.\d+)?%)\s*,\s*(\d+(?:\.\d+)?%)\)/g;
     
-    var reusltadopass = regexpass.exec(valorpass);       
-
-    var resltadocolor = regexpcolor.exec(valorcolor).slice(1);
-     
-    alert("Contraseña :"+reusltadopass+ "\nMatiz: " + resltadocolor[0] + "\nSaturación: " + resltadocolor[1] + "\nValor: " + resltadocolor[2]);      
+    var reusltadopass = regexpass.exec(valorpass).slice();       
     
+    var resltadocolor = regexpcolor.exec(valorcolor).slice(1);
+    if (resltadocolor && reusltadopass){
+        alert("Contraseña :"+ reusltadopass[0] + "\nMatiz: " + resltadocolor[0] + "\nSaturación: " + resltadocolor[1] + "\nValor: " + resltadocolor[2]);      
+  
+    }/*else if (){
+        alert("Los datos son incorectos")
+    }*/
+        
     return false;
 
     
